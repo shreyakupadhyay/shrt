cd0a4d473815527548e53c0090e76bf2c0b798a7
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-08-28 20:14:20 +0500

    Cache template for faster loading :sparkles:

--- runserver.py
+++ runserver.py
@@ -3,4 +3,6 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    # Cache template for fast lodaing
+    app.jinja_env.cache = {}
     app.run(host='0.0.0.0', port=port)
