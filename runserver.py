6a2f8b17261c5527f32ab532a2b380663fac1c84
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-09-05 15:18:06 +0500

    Typo in the comment

--- runserver.py
+++ runserver.py
@@ -3,6 +3,6 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    # Cache template for fast lodaing
+    # Cache template for fast loading
     app.jinja_env.cache = {}
     app.run(host='0.0.0.0', port=port)
