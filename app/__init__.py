963ae4945e70cb2a7e44126c8d48d08d42c33950
itsnauman <nauman-ahmad@outlook.com>
2015-06-13 14:12:41 +0500

    Revert 7aa86ef..46b83a2
    This rolls back to commit 7aa86efa41c98715500b7655c9dbfb89b733e1c5.

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or "secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = db_path
+
 
 db = SQLAlchemy(app)
 
